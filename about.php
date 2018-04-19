dab9fddc2496855cb4c8af2ce75e0f4d2e5af593
Scallen <scallen85@gmail.com>
2018-04-19 13:41:12 -0700

    Changes to index.php and about.php
    Arbitrary changes to see if Github catches the changes

--- about.php
+++ about.php
@@ -5,5 +5,6 @@
 <?php include 'includes/header.php';?>
 <h1><?=$pageID?></h1>
 <p>Retro Burgers are back!</p>
+<p>This was added in our 3rd commit</p>
 
 <?php include 'includes/footer.php';?>
--- index.php
+++ index.php
@@ -5,5 +5,6 @@
 <?php include 'includes/header.php';?>
 <h1><?=$pageID?></h1>
 <img src="images/burger-specials.png" width="960" height="590" alt=""></a>
+<p>Here is some text to test out git</p>
 
 <?php include 'includes/footer.php';?>
